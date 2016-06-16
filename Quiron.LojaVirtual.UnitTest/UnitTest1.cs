243843ca8878b424e3326a60d43a5bea513071ef
Boris Alexandre Sucharov <boris.a.sucha@live.com>
2016-06-16 13:42:16 -0400

    aula 12

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -9,6 +12,29 @@ namespace Quiron.LojaVirtual.UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void testarPaginacaoCorreta()
+        {
+            HtmlHelper html = null;
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+            };
+
+            Func<int, string> paginaUrl = i => "pagina" + i;
+
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            Assert.AreEqual(
+                 @"<a class=""btn btn-default"" href=""pagina1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""pagina2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""pagina3"">3</a>", resultado.ToString()
+
+                );
         }
     }
 }
--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.Web.Controllers
 {
@@ -15,11 +16,27 @@ namespace Quiron.LojaVirtual.Web.Controllers
         public ActionResult ListaProdutos(int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.Produtos
-                .OrderBy(p => p.descricao)
-                .Skip((pagina - 1) * produtosPorPagina)
-                .Take(produtosPorPagina);
-            return View(produtos);
+
+            ProdutosViewModel model = new ProdutosViewModel
+            {
+                Produtos = _repositorio.Produtos
+                    .OrderBy(p => p.descricao)
+                    .Skip((pagina - 1) * produtosPorPagina)
+                    .Take(produtosPorPagina),
+
+
+                Paginacao = new Paginacao
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = produtosPorPagina,
+                    ItensTotal = _repositorio.Produtos.Count()
+
+                }
+            };
+
+
+            return View(model);
+
         }
     }
 }
