2a47c0f60873fc549dc17c96108585a298ba2f5c
Boris Alexandre Sucharov <boris.a.sucha@live.com>
2016-06-20 14:46:34 -0400

    aula 22

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage="Informe seu nome")]
+
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "Cep:")]
+        public String Cep { get; set; }
+
+        [Required(ErrorMessage = "Informe seu endereço")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+
+        [Required(ErrorMessage = "Informe a cidade")]
+        [Display(Name = "Cidade:")]
+        public string Cidade{ get; set; }
+
+        [Required(ErrorMessage = "Informe o bairro")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Informe o Estado")]
+        [Display(Name = "Estado:")]
+        public string Estado { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o email")]
+        [Display(Name = "email:")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        public bool EmbrulhaPresente { get; set; }
+
+    }
+}
---
+++ Quiron.LojaVirtual.UnitTest/TesteCarrinhoCompras.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class TesteCarrinhoCompras
+    {
+        //Teste Adicionar Itens ao Carrinho
+        [TestMethod]
+        public void AdicionarItensAoCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                produtoID = 1,
+                nome = "teste1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                produtoID = 2,
+                nome = "teste2"
+            };
+
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1,2);
+            carrinho.AdicionarItem(produto2, 3);
+            ItemCarrinho[] itens = carrinho.ItensCarrinho.ToArray();
+
+            Assert.AreEqual(itens.Length, 2);
+            Assert.AreEqual(itens[0].Produto, produto1);
+            Assert.AreEqual(itens[1].Produto, produto2);
+        }
+
+        [TestMethod]
+        public void AdicionarProdutoExistente()
+        {
+            Produto produto1 = new Produto
+            {
+                produtoID = 1,
+                nome = "teste1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                produtoID = 2,
+                nome = "teste2"
+            };
+
+            //Produto produto3 = new Produto
+            //{
+            //    produtoID = 3,
+            //    nome = "teste3"
+            //};
+
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 3);
+            carrinho.AdicionarItem(produto1, 10);
+            ItemCarrinho[] resultado = carrinho.ItensCarrinho.OrderBy(c => c.Produto.produtoID).ToArray();
+
+            Assert.AreEqual(resultado.Length, 2);
+            Assert.AreEqual(resultado[0].Quantidade, 11);
+
+        }
+    }
+}
